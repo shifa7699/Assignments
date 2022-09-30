37c82f6290d2abb7311bc6faa676faec25b3660e
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-09-30 14:11:24 +0530

    Add files via upload

---
+++ Shapes.java
@@ -0,0 +1,49 @@
+package com.cognizant.shapes;
+
+public class Shapes {
+	int numberOfSides;
+	void calculateShapeArea(int numberOfsides,int side_length) {
+	this.numberOfSides=numberOfsides;
+	switch(numberOfsides) {
+	case 1:
+		Circle cir=new Circle();
+		cir.calculateArea(side_length);
+		break;
+	case 3:
+		Triangle tri =new Triangle();
+		tri.calculateArea(side_length);
+		break;
+	
+	case 4:
+		Square sq =new Square();
+		sq.calculateArea(side_length);
+		break;
+
+default:
+	System.out.println("No Shapes Present.");
+	}
+
+		/*if(numberOfsides==1) {
+			Circle cir=new Circle();
+			cir.calculateArea(side_length);
+		}
+		else if(numberOfsides==3) {
+			Triangle tri =new Triangle();
+			tri.calculateArea(side_length);
+		}
+		else if(numberOfsides==4) {
+			Square sq =new Square();
+			sq.calculateArea(side_length);
+		}
+		else {
+		
+			System.out.println("No Shapes Present.");
+		}*/
+			
+	}
+	public static void main(String[] args) {
+		new Shapes().calculateShapeArea (5,15);
+		new Shapes().calculateShapeArea (3,12);
+		new Shapes().calculateShapeArea (4,15);
+	}
+}
