33e9a8a210df8e9ed03c1564b8a3b3e21a55072c
shifa7699 <111556272+shifa7699@users.noreply.github.com>
2022-09-30 13:26:14 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,9 @@
+package com.cognizant.shapes;
+
+public class Circle {
+
+public void calculateArea (float radius) {
+	float area=3.14f*radius*radius;
+	System.out.println("The Area of the Circle is= "+area);
+}
+}
